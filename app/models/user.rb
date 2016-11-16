b46412830db82794a983ce662a6cd8feeaa7adef
[01;31m[K"name"[m[K: "John Lin <[01;31m[K"email"[m[K: "johnlin1214@gmail.com>
2016-11-16 13:48:48 -0800

    Added rack:attack gem and authentication api tokens

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
-class User < ApplicationRecord
+class User < ApplicationRecordApplicationRecord
+  before_create do |user|
+    user.api_key = user.generate_api_key
+  end
+
+  # Generate unique API key
+  def generate_api_key
+    loop do
+      token = SecureRandom.base64.tr('+/=', 'Qrt')
+      break token unless User.exists?(api_key: token)
+    end
+  end
 end
